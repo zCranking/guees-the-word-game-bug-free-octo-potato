ae6b163f84e81cb85e1efe1b8fd25679b0a31201
Arav <71561508+zCranking@users.noreply.github.com>
2021-04-14 20:18:10 -0700

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -44,3 +44,40 @@ function send(){
     document.getElementById("word").value = "";
 
 }
+
+var question_turn = "player1";
+var answer_turn = "player2";
+
+function check(){
+    get_answer = document.getElementById("input_check_box").value;
+    answer = get_answer.toLowerCase();
+    console.log("answer in lowercase -" + answer);
+    if(answer == word){
+        if(answer_turn == "player1"){
+            player_1_score = player_1_score + 1;
+            document.getElementById("player1_score").innerHTML = player_1_score;
+        }
+        else{
+            player_2_score = player_2_score + 1;
+            document.getElementById("player2_score").innerHTML = player_2_score;
+        };
+    }
+    if(question_turn == "player1"){
+        question_turn = "player2";
+        document.getElementById("player_question").innerHTML = "Question Turn - " + player_2_name;
+    }
+    else{
+        question_turn = "player1";
+        document.getElementById("player_question").innerHTML = "Question Turn - " + player_1_name;
+    };
+    if(answer_turn == "player2"){
+        answer_turn = "player1";
+        document.getElementById("player_answer").innerHTML = "Answer Turn - " + player_1_name;
+    }
+    else{
+        answer_turn = "player2";
+        document.getElementById("player_answer").innerHTML = "Answer Turn - " + player_2_name;
+    };
+
+    document.getElementById("output").innerHTML = "";
+};
